430b9d0e80035bc2c89da332def6acab98a1082d
PonomareVlad <Git@PonomareVlad.ru>
2022-05-03 00:29:04 +0500

    Isomorphic Async component example with serverUntil()

--- api/index.mjs
+++ api/index.mjs
@@ -1,4 +1,5 @@
 import {render} from "@lit-labs/ssr/lib/render-with-global-dom-shim.js"
+import {readableFrom} from '@lit-labs/ssr/lib/readable.js';
 import {readFileSync} from 'fs'
 import {html} from "lit"
 
@@ -14,7 +15,8 @@ const template = ({url}) => html`
     <my-component url="${url}"><span style="color: coral">Not rendered</span></my-component>`
 
 export default async (req, res) => {
+    const stream = readableFrom(render(template(req)), true)
     res.write(`<!doctype html><html lang="en"><head>${head}${importMap}</head><body>`)
-    for await (const chunk of render(template(req))) res.write(chunk)
-    res.end(`${footer}</body></html>`)
+    stream.on('end', () => res.end(`${footer}</body></html>`))
+    stream.pipe(res, {end: false})
 }
--- components/async-component.mjs
+++ components/async-component.mjs
@@ -1,13 +1,22 @@
 import {LitElement, html} from "lit"
-import {until} from 'lit/directives/until.js'
+import {serverUntil} from "@lit-labs/ssr-client/directives/server-until.js";
 
 export class AsyncComponent extends LitElement {
+    static get properties() {
+        return {ip: {state: true}, isClient: {state: true}}
+    }
+
     async fetchIP() {
-        return await fetch('https://api.ipify.org?format=json').then(r => r.json()).then(({ip}) => ip)
+        return this.ip || (this.ip = await fetch('https://api.ipify.org?format=json').then(r => r.json()).then(({ip}) => ip))
+    }
+
+    firstUpdated(_changedProperties) {
+        this.isClient = true
     }
 
     render() {
-        return html`<span style="color: gray">Worker IP:</span> ${until(this.fetchIP(), 'Loading...')}`
+        return html`<span style="color: gray">Worker IP:</span> ${serverUntil(this.fetchIP())}
+        <span style="color: coral">${this.isClient && !this.ip ? 'Updating...' : ''}</span>`
     }
 }
 
