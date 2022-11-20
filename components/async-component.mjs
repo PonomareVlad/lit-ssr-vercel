c6eb9939215cab546832f9ca8c1a00f94aa3660d
PonomareVlad <Git@PonomareVlad.ru>
2022-11-20 21:11:53 +0500

    Isomorphic async rendering and hydration

--- components/async-component.mjs
+++ components/async-component.mjs
@@ -1,26 +1,13 @@
-import {LitElement, html, nothing, isServer} from "lit"
+import {LitElement, html} from "lit"
 import {serverUntil} from "@lit-labs/ssr-client/directives/server-until.js";
 
 export class AsyncComponent extends LitElement {
-    static get properties() {
-        return {hydrated: {state: true}, ip: {state: true}}
-    }
-
     async fetchIP(apiURL = 'https://api.ipify.org?format=json') {
-        const response = await fetch(apiURL)
-        const {ip} = await response.json()
-        return this.ip = ip
-    }
-
-    firstUpdated() {
-        requestAnimationFrame(() => this.hydrated = true)
+        return await fetch(apiURL).then(r => r.json()).then(({ip}) => ip)
     }
 
     render() {
-        const ip = this.ip || ((isServer || this.hydrated) ? serverUntil(this.fetchIP()) : nothing)
-        const updating = this.hydrated && !this.ip ? 'Updating...' : nothing
-        return html`<span style="color: gray">Worker IP:</span> ${ip}
-        <span style="color: coral">${updating}</span>`
+        return html`<span style="color: gray">Worker IP:</span> ${serverUntil(this.fetchIP(), 'Loading...')}`
     }
 }
 
