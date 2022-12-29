0e21d5d8ae5083fd835b8731bd706761cd5a74a0
Vladislav Ponomarev <Git@PonomareVlad.ru>
2022-12-30 04:42:48 +0500

    Fixed url path

--- api/index.mjs
+++ api/index.mjs
@@ -4,8 +4,13 @@ import {html} from "lit";
 
 import '../components/my-component.mjs';
 
+const getUrlPath = url => {
+    const location = new URL(url);
+    return location.href.replace(location.origin, '')
+}
+
 const template = ({url}) => html`
-    <my-component url="${url}"><span style="color: coral">Not rendered</span></my-component>`
+    <my-component url="${getUrlPath(url)}"><span style="color: coral">Not rendered</span></my-component>`
 
 export default req => render(template(req));
 
