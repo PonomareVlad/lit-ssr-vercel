a2c277a969f01b0998f5f82549457fd3840ea18d
Vladislav Ponomarev <Git@PonomareVlad.ru>
2023-01-03 13:38:08 +0500

    Updated dependencies

--- includes/importmap.mjs
+++ includes/importmap.mjs
@@ -1,13 +1,13 @@
 export default {
-  "imports": {
-    "lit": "https://cdn.jsdelivr.net/npm/@lit-async/lit",
-    "lit/": "https://cdn.jsdelivr.net/npm/@lit-async/lit/",
-    "lit-html": "https://cdn.jsdelivr.net/npm/@lit-async/lit-html",
-    "lit-html/": "https://cdn.jsdelivr.net/npm/@lit-async/lit-html/",
-    "lit-element/": "https://cdn.jsdelivr.net/npm/@lit-async/lit-element@3.2.2-mirror.4/",
-    "@lit-labs/ssr-client/": "https://cdn.jsdelivr.net/npm/@lit-async/ssr-client/",
-    "@lit/reactive-element/": "https://cdn.jsdelivr.net/npm/@lit-async/reactive-element/",
-    "@lit/reactive-element": "https://cdn.jsdelivr.net/npm/@lit-async/reactive-element/reactive-element.js",
-    "@webcomponents/template-shadowroot/": "https://cdn.skypack.dev/@webcomponents/template-shadowroot/"
-  }
+    "imports": {
+        "lit": "https://cdn.jsdelivr.net/npm/@svalit/lit@async",
+        "lit/": "https://cdn.jsdelivr.net/npm/@svalit/lit@async/",
+        "lit-html": "https://cdn.jsdelivr.net/npm/@svalit/lit-html@async",
+        "lit-html/": "https://cdn.jsdelivr.net/npm/@svalit/lit-html@async/",
+        "lit-element/": "https://cdn.jsdelivr.net/npm/@svalit/lit-element@async/",
+        "@lit-labs/ssr-client/": "https://cdn.jsdelivr.net/npm/@svalit/ssr-client@async/",
+        "@lit/reactive-element/": "https://cdn.jsdelivr.net/npm/@svalit/reactive-element@async/",
+        "@lit/reactive-element": "https://cdn.jsdelivr.net/npm/@svalit/reactive-element@async/reactive-element.js",
+        "@webcomponents/template-shadowroot/": "https://cdn.skypack.dev/@webcomponents/template-shadowroot/"
+    }
 }
